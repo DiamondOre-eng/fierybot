2ed0efc1e47a6cc238eaa2a116d9bf2a0a0db018
DiamondOre-eng <75943423+DiamondOre-eng@users.noreply.github.com>
2020-12-31 13:29:12 -0500

    Update Fierybot.py

--- Fierybot.py
+++ Fierybot.py
@@ -2,11 +2,25 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot, bot
 
-client = Bot(command_prefix='?')
+client = Bot(command_prefix=':')
+
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game("Watching over Fiery Warriors"))
     print('Bot is ready!')
 
-client.run("Nzk0MjM3Mzc1Nzg5NTMxMTY3.X-35Mg.ERIQpJ-iYsqFQNBl2rycP2jfK94")
+CHANNEL_ID = 794243325817257984
+
+
+@client.command()
+async def suggest(ctx, *, description):
+    embed = discord.Embed(title='New suggestion!', description=f'Suggested by: {ctx.author.mention}', color=discord.Color.green())
+    embed.add_field(name='Description', value=description)
+    channel = ctx.guild.get_channel(CHANNEL_ID)
+    msg = await channel.send(embed=embed)
+    await msg.add_reaction('👍')
+    await msg.add_reaction('👎')
+    await ctx.send("Successfully sent your suggestion!")
+
+client.run("Nzk0MjM3Mzc1Nzg5NTMxMTY3.X-35Mg.JSrKaA6zIuvIjBQ60hOImwLHDmQ")
