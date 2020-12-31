6e92f69743aa5774873c613e5485e199f4b228f5
DiamondOre-eng <75943423+DiamondOre-eng@users.noreply.github.com>
2020-12-31 11:25:04 -0500

    Add files via upload

---
+++ Fierybot.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot, bot
+
+client = Bot(command_prefix='?')
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game("Watching over Fiery Warriors"))
+    print('Bot is ready!')
+
+client.run("Nzk0MjM3Mzc1Nzg5NTMxMTY3.X-35Mg.zHHRB51bPy0qyECS0orvomvZqoE")
